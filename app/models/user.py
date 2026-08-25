89484f78677f2ccbd8cee006c338b463c1249884
mohammadsoltani1 <mohammadsoltani1br@gmail.com>
2026-08-25 14:11:54 +0330

    add user model

---
+++ app/models/user.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy import DateTime, String, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
