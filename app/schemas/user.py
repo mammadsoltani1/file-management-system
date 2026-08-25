fdb1f6deafdfaa8f7249c4866bf8c6e777f09995
mohammadsoltani1 <mohammadsoltani1br@gmail.com>
2026-08-25 14:35:29 +0330

    feat: add auth and user pydantic schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, EmailStr
+
+
+class UserPublic(BaseModel):
+    model_config=ConfigDict(from_attributes=True)
+    id: UUID
+    email: EmailStr
+    created_at: datetime
